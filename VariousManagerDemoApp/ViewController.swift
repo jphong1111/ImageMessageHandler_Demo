7b77e2540b4e18219616a4981ba3c2444f33f17b
Jungpyo Hong <54448459+jphong1111@users.noreply.github.com>
2021-04-20 03:40:18 -0500

    Initial Commit

---
+++ VariousManagerDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VariousManagerDemoApp
+//
+//  Created by JungpyoHong on 4/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
